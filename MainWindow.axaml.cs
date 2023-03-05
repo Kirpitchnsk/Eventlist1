fc921286779841d9ba94ec391aad92498089e1c3
nskrus054@gmail.com <nskrus054@gmail.com>
2023-03-05 23:43:56 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EventList
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
